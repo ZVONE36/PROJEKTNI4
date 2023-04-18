37c77efbeb0dc4815d5cf7507344a2ee84782903
Zvonimir Škvorc (zskvorc) <zskvorc@tvz.hr>
2023-04-18 16:44:36 +0200

    Projektni4

---
+++ kategorija/ispis_kategorije.py
@@ -0,0 +1,10 @@
+from artikl import ispis_artikla
+def get_kategorija(redni_broj, kategorija):
+    return f"{redni_broj}. {kategorija['naziv']}"
+
+def ispis_svih_kategorija(kategorije):
+    print("Popis kategorija: ")
+    for kategorija in kategorije:
+        print(f"{kategorija['naziv']}")
+        for artikl in kategorija['artikli']:
+            ispis_artikla(artikl)
---
+++ prodaja/ispis_prodaje.py
@@ -0,0 +1,16 @@
+from artikl import ispis_artikla
+from korisnik import ispis_korisnika
+
+
+def ispis_prodaje(prodaja):
+    ispis_korisnika(prodaja['korisnik'])
+    ispis_artikla(prodaja['artikl'])
+    print('Datum isteka:')
+    print(f"\tDan: {prodaja['datum'].day}")
+    print(f"\tMjesec: {prodaja['datum'].month}")
+    print(f"\tGodina: {prodaja['datum'].year}")
+    print('-' * 20)
+
+def ispis_svih_prodaja(prodaje):
+    for prodaja in prodaje:
+        ispis_prodaje(prodaja)
